f98921e52a3828249b1ad28c76db6bcbcd1647e6
Aleksey Kolesnikov <leks.kolesnikov@gmail.com>
2017-05-12 12:09:19 +0300

    Create Users Tests

---
+++ FileSharing/FileSharing.DataAccess.Sql.Tests/UsersRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileSharing.DataAccess.Sql.Tests
+{
+    [TestClass]
+    public class UsersRepositoryTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
