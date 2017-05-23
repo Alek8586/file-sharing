7dea1fa9f1cb45a20e0a59d009e50ce05f6248a1
Aleksey Kolesnikov <leks.kolesnikov@gmail.com>
2017-05-23 17:10:43 +0300

    Create UsersController

---
+++ FileSharing/FileSharing.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FileSharing.DataAccess;
+using FileSharing.Model;
+using FileSharing.DataAccess.Sql;
+
+namespace FileSharing.WebApi.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private const string ConnectionString = "Data Source=DESKTOP-HP;Initial Catalog=FileSharing;Integrated Security=true";
+        private readonly IUsersRepository _usersRepository = new UsersRepository(ConnectionString);
+        private readonly IFilesRepository _filesRepository;
+
+        public UsersController()
+        {
+            _filesRepository = new FilesRepository(ConnectionString, _usersRepository);
+        }
+
+        ///<summary>
+        ///Create user
+        ///</summary>
+        ///<param name="user"></param>
+        ///<returns></returns>
+
+        [HttpPost]
+        public void CreateUser([FromBody]User user)
+        {
+            _usersRepository.Add(user.Name, user.Email);
+        }
+
+        [HttpGet]
+        public User GetUser(Guid id)
+        {
+
+            return _usersRepository.Get(id);
+        }
+
+        [HttpDelete]
+        public void DeleteUser(Guid id)
+        {
+            _usersRepository.Delete(id);
+        }
+
+        [Route("api/users/{id}/files")]
+        [HttpGet]
+        public IEnumerable<File> GetUserFiles(Guid id)
+        {
+            return _filesRepository.GetUserFiles(id);
+        }
+    }
+}
---
+++ FileSharing/FileSharing.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FileSharing.WebApi.WebApiApplication" Language="C#" %>
